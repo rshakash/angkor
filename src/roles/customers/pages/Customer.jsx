e2695add2fd7f9df4d9987a489e4b9a9dc588710
AkshatKumar1609 <akshatk.ug23.cs@nitp.ac.in>
2024-04-14 04:03:48 +0530

    customer navbar done

--- src/roles/customers/pages/Customer.jsx
+++ src/roles/customers/pages/Customer.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 const Farmer = () => {
   return (
-    <div>
-        <h1>Customer</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti saepe explicabo odio dignissimos impedit obcaecati ullam dolores cum officia accusamus, animi quis maxime, itaque sint tenetur voluptate maiores. Eius, dignissimos!</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium amet nisi vitae quae distinctio fugiat ex unde vel assumenda nobis eligendi quos, iste officiis eum commodi similique sed excepturi ipsam?</p>
-        <Link to="/customer/profile">Profile</Link>
-        <Link to="/customer/transactions">Transactions</Link>
-        <Link to="/customer/explore">Explore</Link>
-        <Link to="/customer/home">Home</Link>
-        <Link to="/customer/messages">Messages</Link>
-        <Link to="/customer/search">Search</Link>
+    <nav className="navside">
+        <div id='heading'>
+        <h1 className='font-extrabold text-4xl'>Angkor</h1>
+        </div>
+        <ul className='p-4' id='navholder'> 
+        <li className='navig'><i class="fa-solid fa-house p-1"></i><Link to="/customer/home">Home</Link></li>
+        <li className='navig'><i class="fa-solid fa-user-tie p-1"></i><Link to="/customer/profile">Profile</Link></li>
+        <li className='navig'><i class="fa-solid fa-money-bill-transfer"></i><Link to="/customer/transactions">Transactions</Link></li>
+        <li className='navig'><i class="fa-brands fa-wpexplorer"></i><Link to="/customer/explore">Explore</Link></li>      
+        <li className='navig'><i class="fa-regular fa-message"></i><Link to="/customer/messages">Messages</Link></li>
+        <li className='navig'><i class="fa-solid fa-magnifying-glass p-1"></i><Link to="/customer/search">Search</Link></li>
         <a href='https://angkor-backend.onrender.com/logout'>Logout</a>
-    </div>
+        </ul>
+    </nav>
   )
 }
 
