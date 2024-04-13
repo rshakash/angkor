7e890b24c5e8c4ef30f8e63deab8072bdb51fa0b
Avi2014 <avinashr.ug23.cs@nitp.ac.in>
2024-04-14 03:26:45 +0530

    Edit message component

--- src/roles/customers/pages/Messagesc.jsx
+++ src/roles/customers/pages/Messagesc.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './message.css'
 
 const Messagesc = () => {
   return (
-    <div>Messagesc</div>
+    <div>
+      <div id="messaging-page">
+        <div id="messages">
+            <div className="message">
+                <div className="sender">User 1</div>
+                <div className="content"><input type="text" /></div>
+            </div>
+            <div className="message">
+                <div className="sender">User 2</div>
+                <div className="content"><input type="text" /></div>
+            </div>
+        </div>
+        <div id="message-input">
+            <input type="text" id="message-text" placeholder="Type your message here..." />
+            <button id="send-button">Send</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
