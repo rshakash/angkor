63e5018df65d1e958d77e8ef6d46a4f65ce383ce
Avi2014 <avinashr.ug23.cs@nitp.ac.in>
2024-04-14 13:20:08 +0530

    Navbar

--- src/roles/farmers/Components/Navbar.jsx
+++ src/roles/farmers/Components/Navbar.jsx
@@ -8,12 +8,12 @@ const Navbar = () => {
         <h1 className='font-extrabold text-4xl'>Angkor</h1>
         </div>
         <ul className='p-4' id='navholder'>          
-            <li className='navig'><i class="fa-solid fa-magnifying-glass p-1"></i><Link to="/farmer/search">Search</Link></li>
-            <li className='navig'><i class="fa-solid fa-house p-1"></i>    <Link to="/farmer">Home</Link></li>
-            <li className='navig'><i class="fa-solid fa-cloud p-1"></i><Link to="/farmer/weather">Weather</Link></li>
-            <li className='navig'><i class="fa-solid fa-wheat-awn p-1"></i><Link to="/farmer/cropinfo">CropInfo</Link></li>
-            <li className='navig'><i class="fa-solid fa-user-tie p-1"></i><Link to="/farmer/profile">Profile</Link></li>
-            <li className='navig ' id='logout' ><i class="fa-solid fa-right-from-bracket p-1"></i><a href='https://angkor-backend.onrender.com/logout'>Logout</a></li>
+            <li className='navig'><i className="fa-solid fa-magnifying-glass p-1"></i><Link to="/farmer/search">Search</Link></li>
+            <li className='navig'><i className="fa-solid fa-house p-1"></i>    <Link to="/farmer">Home</Link></li>
+            <li className='navig'><i className="fa-solid fa-cloud p-1"></i><Link to="/farmer/weather">Weather</Link></li>
+            <li className='navig'><i className="fa-solid fa-wheat-awn p-1"></i><Link to="/farmer/cropinfo">CropInfo</Link></li>
+            <li className='navig'><i className="fa-solid fa-user-tie p-1"></i><Link to="/farmer/profile">Profile</Link></li>
+            <li className='navig ' id='logout' ><i className="fa-solid fa-right-from-bracket p-1"></i><a href='https://angkor-backend.onrender.com/logout'>Logout</a></li>
           </ul>
     </nav>
   )
