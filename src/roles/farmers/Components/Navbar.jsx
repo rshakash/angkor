b51d3bdcadfca439909f0ea3b59c4ca18868eb1e
Avi2014 <avinashr.ug23.cs@nitp.ac.in>
2024-04-14 14:54:02 +0530

    message component

--- src/roles/farmers/Components/Navbar.jsx
+++ src/roles/farmers/Components/Navbar.jsx
@@ -10,7 +10,8 @@ const Navbar = () => {
         <ul className='p-4' id='navholder'>          
           <Link to="/farmer/search"><li className='navig'><i className="fa-solid fa-magnifying-glass p-1"></i><span className='hide'>Search</span></li></Link>
           <Link to="/farmer"><li className='navig'><i className="fa-solid fa-house p-1"></i><span className='hide'>Home</span></li></Link>
-          <Link to="/farmer/weather"><li className='navig'><i className="fa-solid fa-cloud p-1"></i><span className='hide'>Weather</span></li></Link>
+          <Link to="/farmer/weather"><li className='navig'><i className="fa-solid fa-cloud p-1"></i><span className='hide'>Weather</span></li></Link>            
+        <Link to="/farmer/messages"><li className='navig'><i className="fa-regular fa-message p-1"></i><span className='hide'>Messages</span></li></Link>
           <Link to="/farmer/cropinfo"><li className='navig'><i className="fa-solid fa-wheat-awn p-1"></i><span className='hide'>CropInfo</span></li></Link>
           <Link to="/farmer/profile"><li className='navig'><i className="fa-solid fa-user-tie p-1"></i><span className='hide'>Profile</span></li></Link>
           <li className='navig ' id='logout' ><i className="fa-solid fa-right-from-bracket p-1"></i><a href='https://angkor-backend.onrender.com/logout'><span className='hide'>Logout</span></a></li>
--- src/roles/farmers/pages/Messagesf.jsx
+++ src/roles/farmers/pages/Messagesf.jsx
@@ -1,14 +1,29 @@
+import React from 'react'
+import './messagef.css'
 import Navbar from '../Components/Navbar'
 
-const Messagesf = () => {
+const Messagesc = () => {
   return (
     <div>
-      <Navbar />
-      <div>
-        Messagesf
+      <Navbar/>
+      <div id="messaging-page">
+        <div id="messages">
+            <div className="message">
+                <div className="sender">User 1</div>
+                <div className="content">Hello, How are u from?</div>
+            </div>
+            <div className="message">
+                <div className="sender">User 2</div>
+                <div className="content">I'm from good, thanks!</div>
+            </div>
+        </div>
+        <div id="message-input">
+            <input type="text" id="message-text" placeholder="Type your message here..." />
+            <button id="send-button">Send</button>
+        </div>
       </div>
     </div>
   )
 }
 
-export default Messagesf
+export default Messagesc
