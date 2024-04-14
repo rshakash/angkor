bc33a07f73f0a59f380b608b6250d70ecbcbb590
rshakash <akashr.ug23.me@nitp.ac.in>
2024-04-14 14:27:00 +0530

    Design navbar for mobile devices

--- src/roles/customers/CustomerComponents/NavbarC.jsx
+++ src/roles/customers/CustomerComponents/NavbarC.jsx
@@ -1,4 +1,3 @@
-import './NavbarC.css'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
@@ -8,13 +7,13 @@ const Navbar = () => {
         <h1 className='font-extrabold text-4xl'>Angkor</h1>
         </div>
         <ul className='p-4' id='navholder'>          
-        <li className='navig'><i className="fa-solid fa-house p-1"></i><Link to="/customer/home">Home</Link></li>
-        <li className='navig'><i className="fa-solid fa-magnifying-glass p-1"></i><Link to="/customer/search">Search</Link></li>
-        <li className='navig'><i className="fa-brands fa-wpexplorer p-1"></i><Link to="/customer/explore">Explore</Link></li>
-        <li className='navig'><i className="fa-solid fa-money-bill-transfer p-1"></i><Link to="/customer/transactions">Transactions</Link></li>             
-        <li className='navig'><i className="fa-regular fa-message p-1"></i><Link to="/customer/messages">Messages</Link></li>
-        <li className='navig'><i className="fa-solid fa-user-tie p-1"></i><Link to="/customer/profile">Profile</Link></li>
-        <li className='logout'><i className="fa-solid fa-right-from-bracket p-1"></i><a href='https://angkor-backend.onrender.com/logout'>Logout</a></li>
+        <Link to="/customer/home"><li className='navig'><i className="fa-solid fa-house p-1"></i><span className='hide'>Home</span></li></Link>
+        <Link to="/customer/search"><li className='navig'><i className="fa-solid fa-magnifying-glass p-1"></i><span className='hide'>Search</span></li></Link>
+        <Link to="/customer/explore"><li className='navig'><i className="fa-brands fa-wpexplorer p-1"></i><span className='hide'>Explore</span></li></Link>
+        <Link to="/customer/transactions"><li className='navig'><i className="fa-solid fa-money-bill-transfer p-1"></i><span className='hide'>Transactions</span></li></Link>             
+        <Link to="/customer/messages"><li className='navig'><i className="fa-regular fa-message p-1"></i><span className='hide'>Messages</span></li></Link>
+        <Link to="/customer/profile"><li className='navig'><i className="fa-solid fa-user-tie p-1"></i><span className='hide'>Profile</span></li></Link>
+        <li className='navig' id='logout'><i className="fa-solid fa-right-from-bracket p-1"></i><a href='https://angkor-backend.onrender.com/logout'><span className='hide'>Logout</span></a></li>
           </ul>
     </nav>
   )
--- src/roles/farmers/Components/Navbar.jsx
+++ src/roles/farmers/Components/Navbar.jsx
@@ -8,12 +8,12 @@ const Navbar = () => {
         <h1 className='font-extrabold text-4xl'>Angkor</h1>
         </div>
         <ul className='p-4' id='navholder'>          
-            <li className='navig'><i className="fa-solid fa-magnifying-glass p-1"></i><Link to="/farmer/search">Search</Link></li>
-            <li className='navig'><i className="fa-solid fa-house p-1"></i>    <Link to="/farmer">Home</Link></li>
-            <li className='navig'><i className="fa-solid fa-cloud p-1"></i><Link to="/farmer/weather">Weather</Link></li>
-            <li className='navig'><i className="fa-solid fa-wheat-awn p-1"></i><Link to="/farmer/cropinfo">CropInfo</Link></li>
-            <li className='navig'><i className="fa-solid fa-user-tie p-1"></i><Link to="/farmer/profile">Profile</Link></li>
-            <li className='navig ' id='logout' ><i className="fa-solid fa-right-from-bracket p-1"></i><a href='https://angkor-backend.onrender.com/logout'>Logout</a></li>
+          <Link to="/farmer/search"><li className='navig'><i className="fa-solid fa-magnifying-glass p-1"></i><span className='hide'>Search</span></li></Link>
+          <Link to="/farmer"><li className='navig'><i className="fa-solid fa-house p-1"></i><span className='hide'>Home</span></li></Link>
+          <Link to="/farmer/weather"><li className='navig'><i className="fa-solid fa-cloud p-1"></i><span className='hide'>Weather</span></li></Link>
+          <Link to="/farmer/cropinfo"><li className='navig'><i className="fa-solid fa-wheat-awn p-1"></i><span className='hide'>CropInfo</span></li></Link>
+          <Link to="/farmer/profile"><li className='navig'><i className="fa-solid fa-user-tie p-1"></i><span className='hide'>Profile</span></li></Link>
+          <li className='navig ' id='logout' ><i className="fa-solid fa-right-from-bracket p-1"></i><a href='https://angkor-backend.onrender.com/logout'><span className='hide'>Logout</span></a></li>
           </ul>
     </nav>
   )
